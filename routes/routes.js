b98ec7875aa121a9687ad95fd0fc6b4798d1ebe5
Swamdidam <Swamdidam@gmail.com>
2018-08-31 09:03:10 +0100

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -39,42 +39,13 @@ const router = express.Router();
 
 
 router.post("/CreateUser", function (req, res) {
-
-// Function for generating random numbers
-//=====================================================
-
-
-    function randomElement (array) {
-        return array[Math.floor(Math.random() * array.length)];
-                }
-                
-                var characters = '1234567890'.split(''),
-                    result = '';
-                for (var i = 0; i < 11; i++) {
-                    result += randomElement(characters);
-                }
-    
-                var mat = result;
-                console.log(mat)
-//========================================================================
     var form = {
         StateOfOrigin:req.body.StateOfOrigin,
         LGA:req.body.LGA,
         MAT:mat
     }
 
-    // createUser.findOne({mat}, (err, user)=>{
-        // if(user){
-        //     console.log('user exist')
-        //     return res.status(200).json({status:400, message:"User with this email already exist"})
-        // }
-        // else if(err){
-        //     console.log('error')
-        //     return res.status(500).json({message:"error"})
-
-        // }
-        // else{
-            return createUser.create(form)
+     return createUser.create(form)
                 .then(doc=>{
                                
                     return res.status(200).json({message: "User created",doc:doc});
@@ -83,39 +54,7 @@ router.post("/CreateUser", function (req, res) {
                     return res.status(500).json({message: "Could not create user", err: err});
                 })
 
-        // }
-
-    // })
-  
 });
-
-//=============================================================================================
-// User login router
-//=============================================================================================
-
-// router.post('/loginUser',(req, res)=>{
-    
-//     if (req.body.email && req.body.password) {
-
-//         User.authenticate(req.body.email, req.body.password, function (err, user) {
-//             if (err) {
-//                 res.json({status: 500, message: "An error occured", err: err});
-//             }
-//             else if(!user){
-//                 res.json({status:400, message:"The Account does not exist"})
-//             }
-//             else{
-//                 res.json({status:200, message:"Welcome back to home page"})
-//             }
-//         })
-        
-//     }
-//     else{
-//         return res.status(400).json({info:"Both email and password are required"})
-//     }
-
-
-// })
 
 //=============================================================================================
 //Updamatg an exismatg user
