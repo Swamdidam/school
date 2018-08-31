c23bee8e832028593d6d55ce7df40d42422b9953
Swamdidam <Swamdidam@gmail.com>
2018-08-31 09:06:41 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -20,38 +20,6 @@ const UserSchema = new mongoose.Schema({
  
 
 });
-
-UserSchema.pre('save', function(next){
-    var User = this;
-    bcrypt.hash(User.password, 10, function(err, hash){
-        if(err){
-            return next(err);
-        }
-        User.password= hash;
-        next()
-    })
-})
-
-UserSchema.statics.authenticate = function (email, password, callback){
-    var User = this
-    User.findOne({ email: email}).exec(function (err, User){
-        if(err){
-            return callback(err)
-        }else if(!User){
-            var err = new Error('User not found');
-            err.status = 401;
-            return callback(err);
-        }
-        bcrypt.compare(password, User.password, function(err, result){
-            if (result === true){
-                return callback(null, User);
-            }
-            else{
-                return callback()
-            }
-            
-        })
-    })
 }
 module.exports = mongoose.model('User', UserSchema);
 
